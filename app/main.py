4f566ae29fd799cb034ac2bea2c13509f5a721eb
umaedi <umaedi.kh.99@gmail.com>
2026-05-11 16:23:01 +0700

    fix: add missing Depends import

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
